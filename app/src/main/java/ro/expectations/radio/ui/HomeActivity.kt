1dc9993e9c9a3d65fd4656717c45012931ed62e6
Adrian Videnie <avidenie@gmail.com>
2018-03-04 22:40:39 +0200

    Add anonymous authentication with Firebase Auth

--- app/src/main/java/ro/expectations/radio/ui/HomeActivity.kt
+++ app/src/main/java/ro/expectations/radio/ui/HomeActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 import ro.expectations.radio.R
 import ro.expectations.radio.data.RadioListViewModel
@@ -13,8 +16,13 @@ import ro.expectations.radio.data.Resource
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: RadioListAdapter
+
+    companion object {
+        private const val TAG = "HomeActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +37,24 @@ class HomeActivity : AppCompatActivity() {
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         recyclerView.setHasFixedSize(true)
 
-        val viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(RadioListViewModel::class.java)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            initRadioList()
+        } else {
+            firebaseAuth.signInAnonymously().addOnCompleteListener(this, { task ->
+                if (task.isSuccessful) {
+                    initRadioList()
+                } else {
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                }
+            })
+        }
+    }
+
+    private fun initRadioList() {
+        val viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+                .create(RadioListViewModel::class.java)
         viewModel.radios.observe(this, Observer { resource ->
             if (resource?.status == Resource.Status.SUCCESS) {
                 adapter.radios = ArrayList(resource.data)
