9305c0edaf3c7e9b09d7d5570833fa5e4995ef18
Adrian Videnie <avidenie@gmail.com>
2018-06-23 17:56:18 +0300

    Initialise Firestore

--- app/src/main/java/ro/expectations/radio/application/CustomApplication.kt
+++ app/src/main/java/ro/expectations/radio/application/CustomApplication.kt
@@ -3,14 +3,27 @@ package ro.expectations.radio.application
 import android.support.multidex.MultiDexApplication
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.firestore.FirebaseFirestore
+
+
 
 class CustomApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
 
+        // Initialise Firebase
         FirebaseApp.initializeApp(this)
 
+        // Set up Firestore
+        val firestore = FirebaseFirestore.getInstance()
+        val settings = FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build()
+        firestore.firestoreSettings = settings
+
+        // Initialise Fresco
         Fresco.initialize(this)
     }
 }
