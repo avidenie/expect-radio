7423703ebe297b328efb15a1d82ccc9d3755bdde
Adrian Videnie <avidenie@gmail.com>
2018-07-03 19:28:02 +0300

    Clean up

--- app/src/main/java/ro/expectations/radio/ui/HomeActivity.kt
+++ app/src/main/java/ro/expectations/radio/ui/HomeActivity.kt
@@ -2,12 +2,8 @@ package ro.expectations.radio.ui
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
-import android.support.v4.media.MediaMetadataCompat
-import android.support.v4.media.session.MediaControllerCompat
-import android.support.v4.media.session.PlaybackStateCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_home.*
 import ro.expectations.radio.R
 import ro.expectations.radio.service.RADIO_BROWSER_SERVICE_ROOT
@@ -15,12 +11,7 @@ import ro.expectations.radio.service.RADIO_BROWSER_SERVICE_ROOT
 
 class HomeActivity : BaseActivity() {
 
-    companion object {
-        private const val TAG = "HomeActivity"
-    }
-
     private lateinit var adapter: RadioListAdapter
-    private val controllerCallback = MediaControllerCallback()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,23 +37,5 @@ class HomeActivity : BaseActivity() {
             }
         })
     }
-
-    override fun onStop() {
-        super.onStop()
-
-        MediaControllerCompat.getMediaController(this)?.unregisterCallback(controllerCallback)
-    }
-
-    private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
-
-        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
-            Log.d(TAG, "MediaControllerCompat.Callback::onMetadataChanged -> ${metadata?.toString()}")
-        }
-
-        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-            Log.d(TAG, "MediaControllerCompat.Callback::onPlaybackStateChanged -> ${state?.toString()}")
-        }
-    }
-
 }
 
