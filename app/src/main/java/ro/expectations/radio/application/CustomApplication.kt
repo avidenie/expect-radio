161fe4f17d924f59ef6cd859596a6f93048ba5c7
Adrian Videnie <avidenie@gmail.com>
2018-07-15 00:18:11 +0300

    Set up multidex

--- app/src/main/java/ro/expectations/radio/application/CustomApplication.kt
+++ app/src/main/java/ro/expectations/radio/application/CustomApplication.kt
@@ -1,13 +1,13 @@
 package ro.expectations.radio.application
 
-import android.app.Application
+import android.support.multidex.MultiDexApplication
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 
-class CustomApplication : Application() {
+class CustomApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
