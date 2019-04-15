2e4a7cde4d23e0947060a7a8174b5bf582c0ac29
Adrian Videnie <avidenie@gmail.com>
2019-04-15 12:29:22 +0300

    Add animated playing icon for currently playing media item

--- app/src/main/java/ro/expectations/radio/MediaItemAdapter.kt
+++ app/src/main/java/ro/expectations/radio/MediaItemAdapter.kt
@@ -1,6 +1,7 @@
 package ro.expectations.radio
 
 import android.graphics.Typeface
+import android.graphics.drawable.*
 import android.media.session.PlaybackState
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.ViewCompat.postOnAnimation
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.list_item_media_item.view.*
 import ro.expectations.radio.MediaItem.Companion.PLAYBACK_STATUS_CHANGED
@@ -37,13 +41,9 @@ class MediaItemAdapter(
             payloads.forEach { payload ->
                 when (payload) {
                     PLAYBACK_STATUS_CHANGED -> {
-                        if (mediaItem.playbackState == PlaybackState.STATE_BUFFERING) {
-                            holder.loading.visibility = View.VISIBLE
-                        } else {
-                            holder.loading.visibility = View.GONE
-                        }
-
-                        updateItemTypeface(holder, mediaItem)
+                        updateTitleTypeface(holder, mediaItem)
+                        updateLoadingIndicator(holder, mediaItem)
+                        updatePlaybackIcon(holder, mediaItem)
                     }
                     else -> fullRefresh = true
                 }
@@ -55,7 +55,9 @@ class MediaItemAdapter(
             holder.titleView.text = mediaItem.title
             holder.subtitleView.text = mediaItem.subtitle
 
-            updateItemTypeface(holder, mediaItem)
+            updateTitleTypeface(holder, mediaItem)
+            updateLoadingIndicator(holder, mediaItem)
+            updatePlaybackIcon(holder, mediaItem)
 
             Glide.with(holder.iconView)
                 .load(mediaItem.iconUri)
@@ -63,13 +65,50 @@ class MediaItemAdapter(
         }
     }
 
-    private fun updateItemTypeface(holder: MediaItemViewHolder, mediaItem: MediaItem) {
+    private fun updateLoadingIndicator(holder: MediaItemViewHolder, mediaItem: MediaItem) {
+        if (mediaItem.playbackState == PlaybackState.STATE_BUFFERING) {
+            holder.loadingIndicator.visibility = View.VISIBLE
+        } else {
+            holder.loadingIndicator.visibility = View.GONE
+        }
+    }
+
+    private fun updatePlaybackIcon(holder: MediaItemViewHolder, mediaItem: MediaItem) {
+        if (mediaItem.playbackState == PlaybackState.STATE_NONE) {
+            if (holder.playingAnimated != null && !holder.playingAnimated.isRunning) {
+                holder.playingAnimated.stop()
+                holder.playingAnimated.clearAnimationCallbacks()
+            }
+            holder.playingIcon.visibility = View.GONE
+        } else {
+            holder.playingIcon.visibility = View.VISIBLE
+            if (mediaItem.playbackState == PlaybackState.STATE_PLAYING) {
+                if (holder.playingAnimated != null && !holder.playingAnimated.isRunning) {
+                    holder.playingAnimated.start()
+                    holder.playingAnimated.registerAnimationCallback(
+                        object : Animatable2Compat.AnimationCallback() {
+                            override fun onAnimationEnd(drawable: Drawable?) {
+                                postOnAnimation(holder.playingIcon) {
+                                    holder.playingAnimated.start()
+                                }
+                            }
+                        }
+                    )
+                }
+            } else {
+                if (holder.playingAnimated != null && holder.playingAnimated.isRunning) {
+                    holder.playingAnimated.stop()
+                    holder.playingAnimated.clearAnimationCallbacks()
+                }
+            }
+        }
+    }
+
+    private fun updateTitleTypeface(holder: MediaItemViewHolder, mediaItem: MediaItem) {
         if (mediaItem.playbackState == PlaybackState.STATE_NONE) {
             holder.titleView.typeface = Typeface.create(holder.titleView.typeface, Typeface.NORMAL)
-            holder.subtitleView.typeface = Typeface.create(holder.subtitleView.typeface, Typeface.NORMAL)
         } else {
             holder.titleView.typeface = Typeface.create(holder.titleView.typeface, Typeface.BOLD)
-            holder.subtitleView.typeface = Typeface.create(holder.subtitleView.typeface, Typeface.BOLD)
         }
     }
 }
@@ -82,11 +121,14 @@ class MediaItemViewHolder(
     val titleView: TextView = view.title
     val subtitleView: TextView = view.subtitle
     val iconView: ImageView = view.icon
-    val loading: FrameLayout = view.loading
+    val loadingIndicator: FrameLayout = view.loading
+    val playingIcon: ImageView = view.playing
+    val playingAnimated: AnimatedVectorDrawableCompat? = AnimatedVectorDrawableCompat.create(view.context, R.drawable.avd_equalizer)
 
     var item: MediaItem? = null
 
     init {
+        playingIcon.setImageDrawable(playingAnimated)
         view.setOnClickListener {
             item?.let { itemClickedListener(it) }
         }
